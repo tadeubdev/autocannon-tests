da542f800077f5298b5f50505165c325653a8240
Tadeu Barbosa <tadeufbarbosa@gmail.com>
2026-03-08 13:27:46 -0300

    first commit

---
+++ spring-ping/src/test/java/com/teste/ping/PingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teste.ping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
