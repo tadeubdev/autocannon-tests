67abce5ba87e89c71b8b0a07bf1889192c8a18a5
Tadeu Barbosa <tadeufbarbosa@gmail.com>
2026-03-21 21:21:00 -0300

    feat: duplicate nest and spring directories

---
+++ nest-api/src/main.ts
@@ -0,0 +1,19 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
+
+async function bootstrap() {
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+    {
+      logger: false,
+    },
+  );
+  await app.listen(process.env.PORT ?? 8080);
+}
+
+void bootstrap();
